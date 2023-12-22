ff9b9f18667d9145147a18459b4e376b6169521e
fptcsm <gianglamweb@gmail.com>
2023-12-22 19:32:24 +0700

    commit

--- src/routes/api/v1/feed.js
+++ src/routes/api/v1/feed.js
@@ -95,4 +95,49 @@ router.get("/v1/feed/video", async (req, res, next) => {
   }
 });
 
+router.get("/v1/feed/story", async (req, res, next) => {
+  try {
+    const offset = parseInt(req.query.offset, 10) || 0;
+    const limit = FEED_LIMIT;
+    const skip = offset * limit;
+
+    let result = [];
+
+    if (req.isAuthenticated()) {
+      result = await NewsFeedService.getNewsFeed(
+        req.user,
+        { follower: req.user._id },
+        skip,
+        limit
+      );
+    } else {
+      result = await PostService.getPosts(
+        null,
+        { privacy: EPrivacy.public },
+        { skip, limit, sort: { createdAt: -1 } }
+      );
+    }
+
+    if (result.length === 0) {
+      return res
+        .status(404)
+        .json({
+          data: [],
+          status_code: 404,
+          error: { message: "No more feed" },
+          success: true,
+          time_stamp: new Date(),
+          message: "No more feed",
+        });
+      return next(ErrorHandler(404, "No more feed."));
+    }
+
+    res.status(200).send(makeResponseJson(result));
+  } catch (e) {
+    console.log("CANT GET FEED", e);
+    next(e);
+  }
+});
+
+
 export default router;
