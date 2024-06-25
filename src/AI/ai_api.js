6896215cc13b605ff23779ddaf96965a927b2b3a
vdat362002 <vandatdinh2@gmail.com>
2024-06-25 21:15:32 +0700

    fix ai

--- src/AI/ai_api.js
+++ src/AI/ai_api.js
@@ -1,6 +1,6 @@
 export async function predictText(text) {
   try {
-    const response = await fetch("http://127.0.0.1:8000/predict", {
+    const response = await fetch(`${process.env.AI_URL}/predict`, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
